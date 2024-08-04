f88a5f90651b235ff7055ec1327b9e7edc3155f9
Giri Shaffaat Al <155008698+grshaff@users.noreply.github.com>
2024-08-04 22:29:02 +0700

    Update error_handler.php

--- error_handler.php
+++ error_handler.php
@@ -1,21 +1,14 @@
 <?php
-// Mengatur custom error handler
 set_error_handler("customErrorHandler");
 
-// Fungsi custom error handler
 function customErrorHandler($errno, $errstr, $errfile, $errline) {
-    // Menangani error
     if (!(error_reporting() & $errno)) {
         return;
     }
-
-    // Redirect ke halaman 404
     http_response_code(404);
     echo "<script>window.open('404.php','_self')</script>";
     exit();
 }
-
-// Mengaktifkan error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
